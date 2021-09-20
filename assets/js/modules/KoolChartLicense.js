ec0a231cb8ea62a51d5ceb9402d2fec1d1e3fa6c
Plugins <82282790+plugins-g@users.noreply.github.com>
2021-09-20 13:13:53 +0200

    V 0.1

---
+++ assets/js/modules/KoolChartLicense.js
@@ -0,0 +1,10 @@
+// -----------------------------------------------------------------------------
+// KoolChart License Key
+//
+// Product Name : KoolChart v6.0
+// License Type : Premium License
+// Product No : 5CDB-61CA-C2A1-181R
+// Authenticated Server : *.koolchart.com,*.codepen.io,*.cdpn.io
+// Expiration Date : *
+//
+var KoolChartLicense = "d8b1c8a49ea5a1db837bba9505cd9f56cc202f12d69363818bac25351379e7c0:3100630b614341312d36432d3242414431432d35313a384c3120522a203a504542203a6f43694e2e4f6e20704c6456633a2e322a2e2c306f2069502e566e3a654b7035652d64506f52632d2e362a2e2c306d206f45634c2e3a747472206143683a63326c306f316f396b302e342a323a3648";
